ebd0268be8e3aef2393c3ab2fa8e7c26bdb5af7c
sautedman <bowersd@email.arizona.edu>
2023-09-12 05:58:17 -0700

    fleshed out what users should see (and how the inner workings should be to make that happen)

--- amalgamate_for_review.py
+++ amalgamate_for_review.py
@@ -1,1 +1,10 @@
 #take a (series of) text(s), pass each word through analyzer, write each word on a line with preceding, following context, outcome of analyzer, index in sentence, sentence length (so reverse indexing is possible), index of sentence, index of text
+
+#Currently: table of words  -> table of sentences
+#           entry           -> sentences containing exact match of entry headword
+           
+#Ideally:   entry           -> sentences containing match to lemma of entry headword
+#so... add a field to the sentences table featuring lemma for every word in the real sentence field
+    #do we also need to include field with headword ids in case of homophony?
+#since we are at it, we might as well include grammatical analysis, gloss
+#so: match in lemma (and id), display full sentence, gloss/lemma(which ever is practical)+grammatical analysis free translation
