a143808cd01eac132e24b342505fd3d282b89c51
sautedman <bowersd@email.arizona.edu>
2023-09-12 05:24:44 -0700

    *Insubstantial grammatical update to comment

--- amalgamate_for_review.py
+++ amalgamate_for_review.py
@@ -1,1 +1,1 @@
-#take a (series of) text, pass each word through analyzer, write each word on a line with preceding, following context, outcome of analyzer, index in sentence, sentence length (so reverse indexing is possible), index of sentence, index of text
+#take a (series of) text(s), pass each word through analyzer, write each word on a line with preceding, following context, outcome of analyzer, index in sentence, sentence length (so reverse indexing is possible), index of sentence, index of text
