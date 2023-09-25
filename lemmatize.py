c88ea73489a52aea31f90007e71926176a31f9a3
sautedman <bowersd@email.arizona.edu>
2023-09-25 09:58:57 -0700

    package imports for setting up alternate output formats

---
+++ lemmatize.py
@@ -0,0 +1,93 @@
+import os
+import argparse
+import re
+import json
+import csv
+import parse
+import readwrite as rw
+import postprocess as pst
+import preprocess as pre
+import engdict as eng
+
+def interlinearize(fst_file, fst_format, pos_regex, gdict, text_in, trans_in):
+    holder = []
+    for i in range(len(text_in)):
+        sub = [[],[],[], [], [], [trans_in[i]]]
+        for w in pre.sep_punct(text_in[i]).split():
+            w=w.lower()
+            p = pst.parser_out_string_dict(parse.parse(os.path.expanduser(fst_file), fst_format, w).decode())
+            best = pst.disambiguate(pst.min_morphs(*p[w]), pst.min_morphs, *p[w])
+            lem = pst.extract_lemma(p[w][best][0], pos_regex)
+            try: gloss = gdict[lem]
+            except KeyError: 
+                gloss = "NODEF" 
+            abbr = re.search('(\w*\s*){0,4}',gloss)[0].lstrip(" 1")
+            if lem: 
+                sub[0].append(w)
+                sub[1].append(p[w][best][0])
+                sub[2].append(lem)
+                sub[3].append(abbr)
+                if (w, gloss) not in sub[4]: sub[4].append((w, gloss))
+            else: 
+                sub[0].append(w)
+                sub[1].append("?")
+                sub[2].append("?")
+                sub[3].append("?")
+        holder.append(sub)
+    return holder
+
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fst_file", help="file path to primary analyser")
+    parser.add_argument("fst_format", help="hfst or xfst")
+    parser.add_argument("pos_regex", help="regexes for part of speech")
+    parser.add_argument("gloss_file", help="file path to translation dictionary")
+    parser.add_argument("text", help="file path to target text")
+    parser.add_argument("trans", help="file path to text translation")
+    parser.add_argument("-o", "--output", dest="o", nargs="?", help="file name/suffix without filetype extension", default="Lemmatization")
+    return parser.parse_args()
+
+def main(fst_file, fst_format, regex_file, gloss_file, text, trans, output):
+    gdict = eng.mk_glossing_dict(*rw.readin(gloss_file))
+    pos_regex = "".join(rw.readin(regex_file))
+    with open(output, 'w') as file_out:
+        l = 0
+        for inter in  interlinearize(fst_file, fst_format, pos_regex, gdict, text, trans):
+            file_out.write(str(l)+'\n')
+            l += 1
+            wrap = 0
+            mainline = []
+            subline = [[],[],[],[]]
+            for i in range(len(inter[0])):
+                m = max(len(inter[0][i]), len(inter[1][i]), len(inter[2][i]), len(inter[3][i]))
+                if wrap + m > 267:
+                    wrap = 0
+                    mainline.append(subline)
+                    subline = [[], [], [], []]
+                wrap += m+1 #+1 because we add a space between words
+                subline[0].append(inter[0][i]+(" "*(m-len(inter[0][i]))))
+                subline[1].append(inter[1][i]+(" "*(m-len(inter[1][i]))))
+                subline[2].append(inter[2][i]+(" "*(m-len(inter[2][i]))))
+                subline[3].append(inter[3][i]+(" "*(m-len(inter[3][i]))))
+                #inter[0][i] = inter[0][i]+(" "*(m-len(inter[0][i])))
+                #inter[1][i] = inter[1][i]+(" "*(m-len(inter[1][i])))
+                #inter[2][i] = inter[2][i]+(" "*(m-len(inter[2][i])))
+                #inter[3][i] = inter[3][i]+(" "*(m-len(inter[3][i])))
+            else: mainline.append(subline)
+            for sl in mainline:
+                file_out.write(" ".join(sl[0])+'\n') 
+                file_out.write(" ".join(sl[1])+'\n') 
+                file_out.write(" ".join(sl[2])+'\n') 
+                file_out.write(" ".join(sl[3])+'\n') 
+                file_out.write('\n')
+            file_out.write(inter[5][0]+'\n') 
+            file_out.write('\n')
+            for x in sorted(inter[4]): file_out.write("\t".join(x)+'\n') 
+            file_out.write('#######'*3)
+            file_out.write('\n')
+            file_out.write('\n')
+
+if __name__ == "__main__":
+    args = parseargs()
+    main(args.fst_file, args.fst_format, args.pos_regex, args.gloss_file, rw.burn_metadata(2, *rw.readin(args.text)), rw.readin(args.trans), args.o)
