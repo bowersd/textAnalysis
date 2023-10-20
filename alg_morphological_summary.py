99136abc26f24344dfd891d7d9e1420ebe4a1b06
sautedman <bowersd@email.arizona.edu>
2023-10-20 11:37:44 -0700

    keeping notes on number adjustments for 2v1 predications (indep order)

--- alg_morphological_summary.py
+++ alg_morphological_summary.py
@@ -48,7 +48,7 @@ def interpret(analysis_in):
             elif analysis_in["suffixes"][0][i:i+2] == ["1", "Pl"]:
                 analysis_in["suffixes"][1][i] = True
                 analysis_in["suffixes"][1][i+1] = True
-                if summary["Head"] == "VTA"  and summary["Order"] != "Cnj" and summary["O"]["Pers"] == "1" and summary["S"]["Pers"] == "2": 
+                if summary["Head"] == "VTA"  and summary["Order"] != "Cnj" and summary["O"]["Pers"] == "1" and summary["S"]["Pers"] == "2": #this is thm2 .*1pl = (2v1pl/2plv1pl)
                     summary["S"]["Num"] = "Pl/2"
                     summary["O"]["Num"] = "Pl"
                 else: summary["S"]["Num"] = "1Pl"
