7e72404408c35e6a5aed287352b75e7e957b2698
sautedman <bowersd@email.arizona.edu>
2023-10-23 06:29:36 -0700

    trimming __main__ instructions, fixing inevitable minor syntax errors

--- alg_morphological_summary.py
+++ alg_morphological_summary.py
@@ -7,7 +7,7 @@ import engdict as eng
 import readwrite as rw
 
 def interpret(analysis_in):
-    summary = {"S":{"Pers":"", "Num":""}, "O":{"Pers":"", "Num":""}, "DerivChain":"", "Head":"", "Order":"", "Neg":"", "Mode":"", "Else": [x for x in analysis_in["preforms"]+analysis_in["clitic"]}
+    summary = {"S":{"Pers":"", "Num":""}, "O":{"Pers":"", "Num":""}, "DerivChain":"", "Head":"", "Order":"", "Neg":"", "Mode":"", "Else": [x for x in analysis_in["preforms"]+analysis_in["clitic"]]}
     inversion = False #if true, S/O will be inverted at end
     #analysis = [x for x in in analysis_in]
     summary["S"]["Pers"] = analysis_in["prefix"][0]
@@ -33,7 +33,7 @@ def interpret(analysis_in):
             summary["O"]["Pers"] = "3"
             if s == "ThmInv": inversion = True
         #} extracting theme sign information end
-        #{getting number information for theme signs/objects
+        #{getting number information for theme signs/objects, also finding inanimate subjects
         elif summary["O"]["Pers"] == "3" and s == "3" and analysis_in["suffixes"][0:1] == ["4"]:
             analysis_in["suffixes"].pop(0)
             summary["O"]["Num"] == "'"
@@ -51,18 +51,18 @@ def interpret(analysis_in):
             summary["S"]["Num"] = "Pl"
             analysis_in["suffixes"].pop(0)
         elif analysis_in["prefix"][0] == "2": 
-            if s = "1" and analysis_in["suffixes"][0:2] == ["Pl"]:
+            if s == "1" and analysis_in["suffixes"][0:2] == ["Pl"]:
                 analysis_in["suffixes"].pop(0)
                 if summary["O"]["Pers"] == "1" and summary["S"]["Pers"] == "2" and not inversion: #this is thm2 .*1pl = (2v1pl/2plv1pl) 
                     summary["S"]["Num"] = "Pl/2"
                     summary["O"]["Num"] = "Pl"
                 else: summary["S"]["Num"] = "1Pl"
-            elif s = "2" and analysis_in["suffixes"][0:1] == ["Pl"]:
+            elif s == "2" and analysis_in["suffixes"][0:1] == ["Pl"]:
                 analysis_in["suffixes"].pop(0)
                 #if summary["O"]["Pers"] == "1" and summary["S"]["Pers"] == "2" and inversion: summary["S"]["Num"] == "Pl"  ## before inversion (thm1sg/thm1pl .*2pl) = (2pl v 1sg/2pl v 1pl), so no need to specify a special case here 
                 #note: there is no further number information in another slot for first persons here ... like theme signs really are object agreement and inversion swoops them into subjecthood (and/or peripheral suffixes are just for 3rd persons)
                 summary["S"]["Num"] = "Pl" 
-        elif analysis_in["prefix"][0] == "3" and and s = "2" and analysis_in["suffixes"][0:1] == ["Pl"]:
+        elif analysis_in["prefix"][0] == "3" and s == "2" and analysis_in["suffixes"][0:1] == ["Pl"]:
             summary["S"]["Num"] = "Pl"
             analysis_in["suffixes"].pop(0)
         #end prefix number obtained}
@@ -95,7 +95,7 @@ def interpret(analysis_in):
                 analysis_in["suffixes"].pop(0)
         else: summary["Else"].append(s)
     #summary["Else"] = [y[0] for x in analysis_in for y in analysis_in[x] if not y[1]]
-    if inversion = True: summary["S"], summary["O"] = summary["O"], summary["S"]
+    if inversion == True: summary["S"], summary["O"] = summary["O"], summary["S"]
     return summary
 
 def analysis_dict(analysis_string):
@@ -151,35 +151,7 @@ def format_summary(wheat, chaff, lemma, **mapping):
 
 if __name__ == "__main__":
     mega_tags = []
-    gdict = eng.mk_glossing_dict(*rw.readin(sys.argv[4]))
-    pos_regex = "".join(rw.readin(sys.argv[3]))
-    for x in sys.argv[7:]:
+    for x in sys.argv[1:]:
         with open(x) as file_in:
             mega_tags.append(yaml.load(file_in))
-    map_dict = {}
-    for d in mega_tags:
-        for x in d:
-            for y in d[x]:
-                for z in d[x][y]:
-                    map_dict[z] = d[x][y][z]
-    tag_set = identify_targets("<>", *[x for x in map_dict])
-    x = lem.interlinearize(sys.argv[1], sys.argv[2], pos_regex, gdict, rw.burn_metadata(2, *rw.readin(sys.argv[5])), rw.readin(sys.argv[6]))
-    for inter in x:
-        for i in range(len(inter[0])):
-            print(inter[0][i])
-            if inter[1][i]=="?":
-                print(inter[1][i])
-                print(inter[2][i])
-                print(inter[3][i])
-                print()
-            else:
-                straight_tags = re.findall("[^\+]*\+", inter[1][i].split(inter[2][i])[0])+re.findall("[^\+]*\+", inter[1][i].split(inter[2][i])[1])
-                wheat, chaff = winnow(straight_tags,  *tag_set)
-                print(format_summary(wheat, chaff, inter[2][i], **map_dict))
-                print(inter[2][i])
-                print(inter[3][i])
-                print()
-
-
-
-
+    for x in mega_tags: print(x)
