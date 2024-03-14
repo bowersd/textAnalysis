72deccad6fb27749ea39100b583345b5adc5a413
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:33 -0700

    game plan for morphosyntactic summary

---
+++ morphosyntactic_summary.py
@@ -0,0 +1,7 @@
+#goal: provide a user friendly description of morphosyntactic analysis
+
+#read in morphosyntax-user friendly pairs to dict (tuple if you want reversible)
+#extract morphosyntax portion of analyzed word
+#check morphosyntax against reference unit, obtain user friendly translation
+
+
