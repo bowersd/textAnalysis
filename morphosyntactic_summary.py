48bd88533c3a404b00943c3ce3f56dc486e167ef
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:33 -0700

    added lexical extraction to game plan

--- morphosyntactic_summary.py
+++ morphosyntactic_summary.py
@@ -1,7 +1,8 @@
 #goal: provide a user friendly description of morphosyntactic analysis
 
 #read in morphosyntax-user friendly pairs to dict (tuple if you want reversible)
-#extract morphosyntax portion of analyzed word
+#extract morphosyntax portion of analyzed word -> delimit where lexical material is
 #check morphosyntax against reference unit, obtain user friendly translation
+#check lexical material against gloss mapping (if available, else, it will just be the lemma)
 
 
