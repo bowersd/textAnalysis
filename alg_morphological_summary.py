b95f7fa9a99e5c0d0ec78088c1e6d580fd6dd800
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:33 -0700

    added a function to establish the lists we want

--- alg_morphological_summary.py
+++ alg_morphological_summary.py
@@ -1,3 +1,6 @@
+import pyyaml
+import re
+
 def winnow(analysis_in, *wheat):
     #translation suite does not cover preverbs, clitics, reduplication, participles, derivational morphology (and others)
     #to allow the translation suite to function, we separate what it can handle from what it can't
@@ -7,3 +10,14 @@ def winnow(analysis_in, *wheat):
         if a in wheat: h.append(a)
         else: chaff.append(a)
     return (h, chaff)
+
+#need to be smart about lemmata
+def identify_targets(sep, *tag_strings):
+    h = []
+    for s in tag_strings:
+        lr = s.split(sep) #sep can be <> for compiling list of relevant tags, or lemma for prepping an analysis for summary
+        for pre in re.findall("[^\+]*\+", lr[0]):
+            if pre not in h: h.append(pre)
+        for suff in re.findall("\+[^\+]*", lr[1]):
+            if suff not in h: h.append(suff)
+    return h
