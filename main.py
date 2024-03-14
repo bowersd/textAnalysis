349380aac9ea03637c056170b742268ff900abc8
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:36 -0700

    another try with hfst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#import hfst
-#import re
-import pyhfst
+import hfst
+#import regex
+#import pyhfst
 
 print("Coming soon: put in a Nishnaabemwin text, get back a (rough) interlinear analysis of the text")
 print("mkizin")
@@ -8,14 +8,14 @@ print("mkizin+NI ... this was hard coded")
 
 def parse_pyhfst(transducer, *strings):
     h = {}
-    parser = pyhfst.HfstInputStream(transducer).read()
+    parser = hfst.HfstInputStream(transducer).read()
     for s in strings: 
         if s not in h: 
             h[s] = []
             p = parser.lookup(s)
             if not p: h[s].append((s+"+?", 0.00))
             else: 
-                for q in p: h[s].append((re.sub("@.*?@", "" ,q[0]), q[1])) #filtering out flag diacritics, which the hfst api does not do as of dec 2023
+                for q in p: h[s].append((regex.sub("@.*?@", "" ,q[0]), q[1])) #filtering out flag diacritics, which the hfst api does not do as of dec 2023
     return h
 
 print(parse_pyhfst("./morphophonologyclitics_analyze.hfstol", "mkizin"))
