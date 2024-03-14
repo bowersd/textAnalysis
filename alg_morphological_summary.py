7261fc9bde6aaecbc65dcf0450b8bead540d12b1
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:33 -0700

    various bug fixes. the test case now runs and produces output, though not the desired output afaik

--- alg_morphological_summary.py
+++ alg_morphological_summary.py
@@ -3,6 +3,8 @@ import re
 import sys
 
 import lemmatize as lem
+import engdict as eng
+import readwrite as rw
 
 def winnow(analysis_in, *wheat):
     #translation suite does not cover preverbs, clitics, reduplication, participles, derivational morphology (and others)
@@ -18,7 +20,7 @@ def winnow(analysis_in, *wheat):
 def identify_targets(sep, *tag_strings):
     h = []
     for s in tag_strings:
-        lr = s.split(sep) #sep can be <> for compiling list of relevant tags, or lemma for prepping an analysis for summary
+        lr = s.split(sep) #sep can be <> for compiling list of relevant tags, or lemma for prepping an analysis for summary: NO! you need all tags for prepping a string, not just all unique tags
         for pre in re.findall("[^\+]*\+", lr[0]):
             if pre not in h: h.append(pre)
         for suff in re.findall("\+[^\+]*", lr[1]):
@@ -44,7 +46,12 @@ def format_summary(wheat, chaff, lemma, **mapping):
 
 
 if __name__ == "__main__":
-    mega_tags = [yaml.load(x) for x in sys.argv[2:]]
+    mega_tags = []
+    gdict = eng.mk_glossing_dict(*rw.readin(sys.argv[4]))
+    pos_regex = "".join(rw.readin(sys.argv[3]))
+    for x in sys.argv[7:]:
+        with open(x) as file_in:
+            mega_tags.append(yaml.load(file_in))
     map_dict = {}
     for d in mega_tags:
         for x in d:
@@ -52,6 +59,21 @@ if __name__ == "__main__":
                 for z in d[x][y]:
                     map_dict[z] = d[x][y][z]
     tag_set = identify_targets("<>", *[x for x in map_dict])
+    x = lem.interlinearize(sys.argv[1], sys.argv[2], pos_regex, gdict, rw.burn_metadata(2, *rw.readin(sys.argv[5])), rw.readin(sys.argv[6]))
+    for inter in x:
+        for i in range(len(inter[0])):
+            print(inter[0][i])
+            if inter[1][i]=="?":
+                print(inter[1][i])
+                print(inter[2][i])
+                print(inter[3][i])
+            else:
+                straight_tags = re.findall("[^\+]*\+", inter[1][i].split(inter[2][i])[0])+re.findall("[^\+]*\+", inter[1][i].split(inter[2][i])[1])
+                wheat, chaff = winnow(straight_tags,  *tag_set)
+                print(format_summary(wheat, chaff, inter[2][i], **map_dict))
+                print(inter[2][i])
+                print(inter[3][i])
+
 
 
 
