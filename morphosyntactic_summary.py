c14522f95779d7c1d5323fa41c88d341526e2e64
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:33 -0700

    note emphasizing use of delimiters in MSDs

--- morphosyntactic_summary.py
+++ morphosyntactic_summary.py
@@ -1,6 +1,7 @@
 #goal: provide a user friendly description of morphosyntactic analysis
 
 #read in morphosyntax-user friendly pairs to dict (tuple if you want reversible)
+    #use left and right delimiters to mark where root/stem/lexical information goes in the morphosyntactic description (msd)
 #extract morphosyntax portion of analyzed word -> delimit where lexical material is
 #check morphosyntax against reference unit, obtain user friendly translation
 #check lexical material against gloss mapping (if available, else, it will just be the lemma)
