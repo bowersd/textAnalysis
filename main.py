be6a7b87d68b0546b6fe94c8f1211a4cae7ac1f1
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:36 -0700

    altering file upload to use this page's variables

--- main.py
+++ main.py
@@ -50,37 +50,28 @@ from pyodide.ffi import create_proxy
  
 def read_complete(event):
   # event is ProgressEvent
- 
   content = document.getElementById("content");
   content.innerText = event.target.result
 
  
 async def process_file(x):
-  fileList = document.getElementById('upload').files
- 
+  fileList = document.getElementById('targetLanguageText').files
   for f in fileList:
     # reader is a pyodide.JsProxy
     reader = FileReader.new()
- 
     # Create a Python proxy for the callback function
     onload_event = create_proxy(read_complete)
-
     #console.log("done")
- 
     reader.onload = onload_event
- 
     reader.readAsText(f)
- 
   return
  
-def main():
+def main(x):
   # Create a Python proxy for the callback function
   file_event = create_proxy(process_file)
  
   # Set the listener to the callback
-  e = document.getElementById("upload")
+  e = document.getElementById("targetLanguageText")
   e.addEventListener("change", file_event, False)
  
-main()
-   
 
