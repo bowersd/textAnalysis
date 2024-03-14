fe53cdeaa210d8086618cc45b8a1ba1ed745a480
sautedman <sautedman@protonmail.com>
2024-03-14 14:01:36 -0700

    updated diagnostic print statement

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pyhfst
 print("Coming soon: put in a Nishnaabemwin text, get back a (rough) interlinear analysis of the text")
 print("mkizin")
 print("mkizin+NI ... this was hard coded")
-print("tiist")
+print("taast")
 
 def parse_pyhfst(transducer, *strings):
     h = {}
