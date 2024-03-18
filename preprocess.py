a9d240cad77dc9fa88a84bab68975e78ac116ba9
sautedman <sautedman@protonmail.com>
2024-03-18 16:06:22 -0700

    curly and square brackets peeled off

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,7 @@ import re
 
 def sep_punct(string):
     #return "'".join(re.sub("(“|\()", "\g<1> ", re.sub("(”|…|\)|:|;|,|\*|\.|\?|!|/)", " \g<1>", string)).split("’")) #first separate trailing punc, then leading punc, then replace single quote ’ with '
-    return "'".join(re.sub("(\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
+    return "'".join(re.sub("(\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
 
 def sent_break(*args):
     holder = []
