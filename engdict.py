fb6bf2bf573f089a921bbd88e3ec524547183dc8
sautedman <sautedman@protonmail.com>
2024-03-25 12:53:49 -0700

    alternative treatment of homophones

--- engdict.py
+++ engdict.py
@@ -4,7 +4,8 @@ def mk_glossing_dict(*strings):
     for s in strings:
         chunked = s.split("\t")
         if chunked[0] not in gd: gd[chunked[0]] = chunked[1]
-        else: gd[chunked[0]] = gd[chunked[0]] + " HOMOPHONE DEFINITION>" + chunked[1]
+        #else: gd[chunked[0]] = gd[chunked[0]] + " HOMOPHONE DEFINITION>" + chunked[1]
+        else: gd[chunked[0]] = gd[chunked[0]] + "*"
     return gd
 
 def gloss_fix(**gloss_dict):
