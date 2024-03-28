ace33c1d5a3f3a82df0939ebe42fccd6504d25a3
sautedman <sautedman@protonmail.com>
2024-03-28 16:54:16 -0700

    replaced punc with whitespace instead of nothing

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def parse_words(event):
 
 def sep_punct(string, drop_punct): #diy tokenization, use nltk?
     if not drop_punct: return "'".join(regex.sub("(\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
-    return "'".join(regex.sub("(\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", "", string).split("’")) #remove all punc, then replace single quote ’ with '
+    return "'".join(regex.sub("(\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " ", string).split("’")) #remove all punc, then replace single quote ’ with '
 
 def min_morphs(*msds):
     """the length of the shortest morphosyntactic description"""
