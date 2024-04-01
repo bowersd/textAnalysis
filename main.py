735b315c8f3a48c6d2274d4b939d3b3d071396b7
sautedman <sautedman@protonmail.com>
2024-04-01 12:10:54 -0700

    made a longer header for the original word line (helps visually)

--- main.py
+++ main.py
@@ -297,7 +297,7 @@ def parse_words(event):
         except KeyError:
             gloss = "?"
         tinies.append("'"+gloss+"'")
-    padded = pad(["Word:"] + sep_punct(freeNish.lower(), True).split(), ["Narrow Analysis:"] + m_parse_lo, ["Broad Analysis:"] + m_parse_hi, ["Dictionary Header:"] + lemmata, ["Terse Translation:"] + tinies)
+    padded = pad(["Original Text:"] + sep_punct(freeNish.lower(), True).split(), ["Narrow Analysis:"] + m_parse_lo, ["Broad Analysis:"] + m_parse_hi, ["Dictionary Header:"] + lemmata, ["Terse Translation:"] + tinies)
     words_out = "\n".join(["\t".join(p) for p in padded])
     #words_out = tabulate.tabulate([["Word:"] + sep_punct(freeNish.lower(), True).split(), ["Narrow Analysis:"] + m_parse_lo, ["Broad Analysis:"] + m_parse_hi, ["Dictionary Header:"] + lemmata, ["Terse Translation:"] + tinies], tablefmt='html')
     output_div = pyscript.document.querySelector("#output")
