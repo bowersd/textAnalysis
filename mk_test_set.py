10acf8d6a8a8e625e74c14e56b537225a33660a7
sautedman <sautedman@protonmail.com>
2024-06-18 14:55:48 -0700

    user input

--- mk_test_set.py
+++ mk_test_set.py
@@ -14,6 +14,7 @@ while i < len(initial_file):
     if initial_file[i].startswith("Sentence number:"):
         need_user = False
         target = ""
+        sentence_id = re.search("[0-9]+", initial_file[i])[0]
     if (initial_file[i].startswith("Terse translation") and re.search("y/n):.*[yYf]", initial_file[i])) or (initial_file[i+1].startswith("Grammatical analysis") and re.search("y/n):.*[yYf]", initial_file[i+1])): need_user = True
     if initial_file[i].startswith("Target word"): 
         target = initial_file[i].split()[-1]
@@ -23,10 +24,14 @@ while i < len(initial_file):
         data["generated"] = initial_file[i+2].split()[field]
         data["analysis"] = initial_file[i+3].split()[field]
     if target and target in initial_file[i] and all([re.match('[0-9]+', x) for x in initial_file[i].split()]) and need_user:
-        print( initial_file[i+1].split()[field])
-        print( initial_file[i+2].split()[field])
-        print( initial_file[i+3].split()[field])
+        print("written: ", initial_file[i+1].split()[field])
+        print("generated: ", initial_file[i+2].split()[field])
+        print("analysis: ", initial_file[i+3].split()[field])
+        print("sentence id: ", sentence_id)
         #print the original sentence id (so you need to store that), prompt the user for the values to written, generated, analysis
+        data["written"] = initial_file[i+1].split()[field]
+        data["generated"] = input("what should the generated string be? ")
+        data["analysis"] = input("what should the analysis be? ")
     if all(data[x] for x in data): 
         h.append(data)
         data = { "written" : "", "analysis" : "", "generated" : "", }
