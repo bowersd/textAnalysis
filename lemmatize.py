b7583c0b066409dc8b75751657da35a59ae77c23
sautedman <sautedman@protonmail.com>
2024-07-15 10:18:04 -0700

    added file type extensions to spot checks

--- lemmatize.py
+++ lemmatize.py
@@ -366,12 +366,12 @@ if __name__ == "__main__":
                 cnt = 0
                 loci = [(i, j) for j in range(len(full["m_parse_lo"][i])) for i in range(len(full["m_parse_lo"]))]
                 if s[0] == "all" and s[1] == "unanalyzed": 
-                    with open('spot_checks_{0}_{1}_{2}_reversed.txt'.format(s[0], s[1], date.today()), 'w') as fileOut:
-                    with open('spot_checks_{0}_{1}_{2}'.format(s[0], s[1], date.today()), 'w') as fileOut:
+                    with open('spot_checks_{0}_{1}_{2}_reversed.csv'.format(s[0], s[1], date.today()), 'w') as fileOut: pass
+                    with open('spot_checks_{0}_{1}_{2}.csv'.format(s[0], s[1], date.today()), 'w') as fileOut:
                         pass
                 elif s[0] == "all" and s[1] != "unanalyzed": print("full listing of analyzed forms is probably not informative, and is not currently supported")
                 else:
-                    with open('spot_checks_{0}_{1}_{2}'.format(s[0], s[1], date.today()), 'a') as fileOut:
+                    with open('spot_checks_{0}_{1}_{2}.txt'.format(s[0], s[1], date.today()), 'a') as fileOut:
                         while cnt < int(s[0]) and loci:
                             cnt += 1
                             locus = loci.pop(random.randrange(0, len(loci)))
