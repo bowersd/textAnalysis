faec74e8f8d2004dfed65168eb0a73bdc18a39f1
sautedman <sautedman@protonmail.com>
2024-07-16 10:04:22 -0700

    fixed small indexing problem

--- lemmatize.py
+++ lemmatize.py
@@ -322,7 +322,8 @@ if __name__ == "__main__":
                 updates["edited"]= cdict[x[0]][0]
                 updates["lemmata"]= lemmatize(pos_regex, best)[0]
                 updates["analysis_src"]= ["analyzed", "hand"]
-                if x[0] in e_dict and cdict[x[0]][1] != e_dict[x[0]][1][0][0]: print("{0} error analysis {1} manually overwritten by {2}".format(x[0], e_dict[x[0]][1][pst.disambiguate(pst.min_morphs(*pst.minimal_filter(*e_dict[x[0]])), pst.min_morphs, *pst.minimal_filter(*e_dict[x[0]][1]))][0], best)) #AS OF 7/15/2024 MANUAL OVERRIDE OF ANALYSIS ONLY HAPPENS FOR ERROR MODEL, NOT BASE MODEL (EDITED FORM SHOULD ALSO JUST GET RE-WRITTEN BY DEFAULT TOO)
+                if x[0] in e_dict and cdict[x[0]][1] != e_dict[x[0]][1][0][0]: 
+                    print("{0} error analysis {1} manually overwritten by {2}".format(x[0], e_dict[x[0]][1][pst.disambiguate(pst.min_morphs(*pst.minimal_filter(*e_dict[x[0]][1])), pst.min_morphs, *pst.minimal_filter(*e_dict[x[0]][1]))][0], best)) #AS OF 7/15/2024 MANUAL OVERRIDE OF ANALYSIS ONLY HAPPENS FOR ERROR MODEL, NOT BASE MODEL (EDITED FORM SHOULD ALSO JUST GET RE-WRITTEN BY DEFAULT TOO)
             elif args.e and args.g:
                 #if not e_dict[x[0]][1][0][0].endswith('+?'): 
                 if x[0] in e_dict: 
