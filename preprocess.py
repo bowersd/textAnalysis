3ea6fc165987879b4acc7814050dd8f3e105ff14
sautedman <sautedman@protonmail.com>
2024-07-18 11:58:40 -0700

    modifications to handle quotes at word edges better

--- preprocess.py
+++ preprocess.py
@@ -4,12 +4,12 @@ import re
 
 def sep_punct(string, drop):
     #return "'".join(re.sub("(“|\()", "\g<1> ", re.sub("(”|…|\)|:|;|,|\*|\.|\?|!|/)", " \g<1>", string)).split("’")) #first separate trailing punc, then leading punc, then replace single quote ’ with '
-    if not drop: return "'".join(re.sub("(\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
-    return "'".join(re.sub("(\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " ", string).split("’")) #remove all punc, then replace single quote ’ with '
+    if not drop: return "'".join(re.sub(r"(' | '(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
+    return "'".join(re.sub(r"(' | '(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " ", string).split("’")) #remove all punc, then replace single quote ’ with '
 
 def sent_break(*args):
     holder = []
     for a in args:
-        for s in re.split("((\.|!|\?) )", a):
+        for s in re.split(r"((\.|!|\?) )", a):
             holder.append(s)
     return holder
