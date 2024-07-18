9df2f884c49d5053af3cc6a176c3c5c870cdf6a4
sautedman <sautedman@protonmail.com>
2024-07-18 13:38:34 -0700

    added long dash to preprocessing

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,8 @@ import re
 
 def sep_punct(string, drop):
     #return "'".join(re.sub("(“|\()", "\g<1> ", re.sub("(”|…|\)|:|;|,|\*|\.|\?|!|/)", " \g<1>", string)).split("’")) #first separate trailing punc, then leading punc, then replace single quote ’ with '
-    if not drop: return "'".join(re.sub(r"(' | '(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
-    return "'".join(re.sub(r"(' | '(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|…|:|;|,|\*|\.|\?|!|/)", " ", string).split("’")) #remove all punc, then replace single quote ’ with '
+    if not drop: return "'".join(re.sub(r"('+ |(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string).split("’")) #separate all punc, then replace single quote ’ with '
+    return "'".join(re.sub(r"('+ |(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", " ", string).split("’")) #remove all punc, then replace single quote ’ with '
 
 def sent_break(*args):
     holder = []
