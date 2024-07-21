b0b61ac35e136bd598cf9f3a1683d18ab155b8bb
sautedman <sautedman@protonmail.com>
2024-07-21 14:05:07 -0700

    commented out the context specific quote separation, which was causing unequal lengths between chunked and m_parse_lo

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,10 @@ import re
 
 def sep_punct(string, drop):
     #return "'".join(re.sub("(“|\()", "\g<1> ", re.sub("(”|…|\)|:|;|,|\*|\.|\?|!|/)", " \g<1>", string)).split("’")) #first separate trailing punc, then leading punc, then replace single quote ’ with '
-    if not drop: return "'".join(re.split("‘|’", re.sub(r"('+($| )|(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string))) #separate all punc, then replace single quote ’ with '
-    return "'".join(re.split("‘|’", re.sub(r"('+($| )|(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", " ", string))) #remove all punc, then replace single quote ’ with '
+    if not drop: return "'".join(re.split("‘|’", re.sub(r"(\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string))) #separate all punc, then replace single quote ’ with '
+    return "'".join(re.split("‘|’", re.sub(r"(\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", " ", string))) #remove all punc, then replace single quote ’ with '
+    #if not drop: return "'".join(re.split("‘|’|'", re.sub(r"('+($| )|(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", r" \g<1> ", string))) #separate all punc, then replace single quote ’ with '
+    #return "'".join(re.split("‘|’|'", re.sub(r"('+($| )|(^| )'+(?=[^aeioAEIO])|\{|\}|\[|\]|\"|“|\(|\)|”|—|…|:|;|,|\*|\.|\?|!|/)", " ", string))) #remove all punc, then replace single quote ’ with '
 
 def sent_break(*args):
     holder = []
