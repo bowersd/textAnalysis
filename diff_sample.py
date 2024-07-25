d4dbf11771198e3b9ec9c7ef728b7746ab604bd6
sautedman <sautedman@protonmail.com>
2024-07-25 13:42:24 -0700

    write out sample for each version

--- diff_sample.py
+++ diff_sample.py
@@ -4,16 +4,17 @@ import random
 import date
 
 if __name__ == "__main__":
+    versions = {}
     with open(sys.argv[1]) as oldjson:
-        old = json.load(oldjson)
+        versions["old"] = json.load(oldjson)
     with open(sys.argv[2]) as newjson:
-        new = json.load(newjson)
+        versions["new"] = json.load(newjson)
     difftypes = {"change":[], "gain":[], "lose":[]}
-    diffloci = [(i, j) for i in range(len(old)) for j in range(len(old[i]["m_parse_lo"])) if old[i]["m_parse_lo"][j] != new[i]["m_parse_lo"][j]]
+    diffloci = [(i, j) for i in range(len(versions["old"])) for j in range(len(versions["old"][i]["m_parse_lo"])) if versions["old"][i]["m_parse_lo"][j] != versions["new"][i]["m_parse_lo"][j]]
     for dl in diffloci:
-        if not any([new[locus[0]]["m_parse_lo"][locus[1]].endswith('+?'), old[locus[0]]["m_parse_lo"][locus[1]].endswith('+?')]): difftypes["change"].append(dl)
-        elif old[locus[0]]["m_parse_lo"][locus[1]].endswith('+?'): difftypes["gain"].append(dl)
-        else new[locus[0]]["m_parse_lo"][locus[1]].endswith('+?'): difftypes["lose"].append(dl)
+        if not any([versions["new"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?'), versions["old"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?')]): difftypes["change"].append(dl)
+        elif versions["old"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?'): difftypes["gain"].append(dl)
+        else: difftypes["lose"].append(dl)
     print("diff count {0}".format(sum([len(difftypes[x]) for x in difftypes])))
     for dt in difftypes: 
         print("{0} count {1}".format(dt, len(difftypes[dt])))
@@ -23,7 +24,11 @@ if __name__ == "__main__":
             cnt += 1
             locus = difftypes[dt].pop(random.randrange(0, len(difftypes[dt])))
             h.append(locus)
-        for v in ['new', 'old']:
-            with open("diff_check_{0}_{1}_{2}.txt".format(dt, v, date.today()), 'w') as file_out: pass
-        for locus in h: pass
+        for v in versions:
+            with open("diff_check_{0}_{1}_{2}.txt".format(dt, v, date.today()), 'w') as file_out:
+                for locus in h: 
+                    padded = lemmatize.pad(versions[v][locus[0]]["chunked"], versions[v][locus[0]]["m_parse_lo"], versions[v][locus[0]]["m_parse_hi"], versions[v][locus[0]]["tiny_gloss"], versions[v][locus[0]]["english"])
+                    for p in padded: file_out.write(p+"\n")
+                    file_out.write("\n")
+
 
