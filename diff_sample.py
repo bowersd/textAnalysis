2b81730c163fd40917c538be0e39db9ad647bbee
sautedman <sautedman@protonmail.com>
2024-08-01 13:49:05 -0700

    added information on where the current analysis comes from

--- diff_sample.py
+++ diff_sample.py
@@ -1,7 +1,8 @@
 import sys
 import json
 import random
-import date
+from datetime import date
+import lemmatize
 
 if __name__ == "__main__":
     versions = {}
@@ -12,8 +13,9 @@ if __name__ == "__main__":
     difftypes = {"change":[], "gain":[], "lose":[]}
     diffloci = [(i, j) for i in range(len(versions["old"])) for j in range(len(versions["old"][i]["m_parse_lo"])) if versions["old"][i]["m_parse_lo"][j] != versions["new"][i]["m_parse_lo"][j]]
     for dl in diffloci:
-        if not any([versions["new"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?'), versions["old"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?')]): difftypes["change"].append(dl)
-        elif versions["old"][locus[0]]["m_parse_lo"][locus[1]].endswith('+?'): difftypes["gain"].append(dl)
+        #print(versions["old"][dl[0]]["m_parse_lo"][dl[1]])
+        if not any([versions["new"][dl[0]]["m_parse_lo"][dl[1]].endswith('+?'), versions["old"][dl[0]]["m_parse_lo"][dl[1]].endswith('+?')]): difftypes["change"].append(dl)
+        elif versions["old"][dl[0]]["m_parse_lo"][dl[1]].endswith('+?'): difftypes["gain"].append(dl)
         else: difftypes["lose"].append(dl)
     print("diff count {0}".format(sum([len(difftypes[x]) for x in difftypes])))
     for dt in difftypes: 
@@ -28,23 +30,24 @@ if __name__ == "__main__":
             with open("diff_check_{0}_{1}_{2}.txt".format(dt, v, date.today()), 'w') as file_out:
                 for locus in h: 
                     padded = lemmatize.pad([str(i) for i in range(len(versions[v][locus[0]]["chunked"]))], versions[v][locus[0]]["chunked"], versions[v][locus[0]]["m_parse_lo"], versions[v][locus[0]]["m_parse_hi"], versions[v][locus[0]]["tiny_gloss"])
-                    #for p in padded: #file_out.write(p+"\n")
+                    #for p in padded: print(p)#file_out.write(p+"\n")
                     start = 0
                     stop = 0
                     while stop < len(padded[0]):
                         while stop < len(padded[0]) and len(" ".join(padded[0][start:stop])) < 100:
                             stop += 1
                         for p in padded: 
-                            fileOut.write(" ".join(p[start:stop])+'\n')
+                            file_out.write(" ".join(p[start:stop])+'\n')
                         start = stop
-                        if stop < len(padded[0])-1: fileOut.write('\n')
+                        if stop < len(padded[0])-1: file_out.write('\n')
                     file_out.write(versions[v][locus[0]]["english"]+'\n')
                     file_out.write("\n")
         with open("diff_check_{0}_{1}_{2}_report.txt".format(dt, v, date.today()), 'w') as report:
-            report.write("For the purposes of this report 'correct' means hi/lo level grammatical analysis and terse gloss fit English translation")
+            report.write("For the purposes of this report 'correct' means hi/lo level grammatical analysis and terse gloss fit English translation"+"\n")
             for locus in h:
                 report.write("Sentence ID: {0}".format(versions["old"][locus[0]]["sentenceID"])+'\n')
-                report.write("Credit/blame: {0}".format(versions["new"][locus[0]]["analysis_src"][locus[1]][-1])+'\n')
+                if dt == "gain" or dt == "change": report.write("Credit/blame: {0}".format(versions["new"][locus[0]]["analysis_src"][locus[1]][-1])+'\n')
+                if dt == "lose": report.write("Credit/blame: {0}".format(versions["old"][locus[0]]["analysis_src"][locus[1]][-1])+'\n')
                 report.write("Target word: {0} (index: {1})".format(versions["old"][locus[0]]["chunked"][locus[1]], str(locus[1]))+'\n')
                 if dt == "gain" or dt == "change": report.write("Is {0} (new analysis) correct? (y/n)".format(versions["new"][locus[0]]["m_parse_lo"][locus[1]])+'\n')
                 if dt == "change" or dt == "lose": 
