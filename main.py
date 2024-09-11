fda7a62738414e01c87b50f5280b210a148d94f1
sautedman <sautedman@protonmail.com>
2024-09-11 10:20:24 -0700

    appending to div

--- main.py
+++ main.py
@@ -342,7 +342,7 @@ def parse_words_expanded(event):
         #padded = pad(["Original Material:"] + sep_punct(freeNish.lower(), True).split(), ["Narrow Analysis:"] + m_parse_lo, ["Broader Analysis:"] + m_parse_hi, ["Dictionary Entry:"] + lemmata, ["Terse Translation:"] + tinies)
         #words_out = "\n".join(["\t".join(p) for p in padded])
         words_out = tabulate.tabulate([["Original Material:"] + sep_punct(freeNish.lower(), True).split(), ["Narrow Analysis:"] + m_parse_lo, ["Broad Analysis:"] + m_parse_hi, ["Dictionary Header:"] + lemmata, ["Terse Translation:"] + tinies], tablefmt='html')
-        output_div.innerText = words_out 
+        output_div.append(words_out)
     if analysis_mode.value == "frequency":
         cnts = {w:0 for w in sep_punct(freeNish.lower(), True).split()}
         for w in sep_punct(freeNish.lower(), True).split(): cnts[w] += 1
