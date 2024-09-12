f1774a3c5475bc5aaab72ca9772f700c34827c3c
sautedman <sautedman@protonmail.com>
2024-09-11 22:35:33 -0700

    testing index error

--- main.py
+++ main.py
@@ -334,7 +334,7 @@ def parse_words_expanded(event):
         for w in sep_punct(line, True).split():
             if analyzed[w][0][0].endswith('+?'): local.append(re_analyzed[w][disambiguate(min_morphs(*re_analyzed[w]), min_morphs, *re_analyzed[w])][0])
             else: local.append(analyzed[w][disambiguate(min_morphs(*analyzed[w]), min_morphs, *analyzed[w])][0])
-        h["original"].append(line)
+        h["original"].append(sep_punct(line, True).split())
         h["m_parse_lo"].append(local)
         h["m_parse_hi"].append(["'"+formatted(interpret(analysis_dict(x)))+"'" if analysis_dict(x) else "'?'" for x in local])
         h["lemmata"].append([x if x else "?" for x in lemmatize(pos_regex, *local)])
@@ -351,7 +351,7 @@ def parse_words_expanded(event):
         lines_out = ""
         for i in range(len(h["m_parse_lo"])):
             lines_out += tabulate.tabulate([
-                ["Original Material:"] + sep_punct(h["original"][i], True).split(), 
+                ["Original Material:"] + h["original"],
                 ["Narrow Analysis:"] + h["m_parse_lo"][i], 
                 ["Broad Analysis:"] + h["m_parse_hi"][i], 
                 ["Dictionary Header:"] + h["lemmata"][i], 
