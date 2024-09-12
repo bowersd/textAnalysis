0a41872c8fcbeffba5ea8b02a35bb12353e8460c
sautedman <sautedman@protonmail.com>
2024-09-11 22:49:49 -0700

    header vs headers

--- main.py
+++ main.py
@@ -390,7 +390,7 @@ def parse_words_expanded(event):
                     recall_errors.append(error)
         forwards = ""
         for r in sorted(recall_errors):
-            forwards += tabulate.tabulate([r[0][0], r[0][1], ["", ""]+r[1], ["", ""]+r[2], ["", ""]+r[3], ["", ""]+r[4], ["", ""]+r[5]], header = ["error", "sentence_number", "left_context", "location", "right_context"], tablefmt = "html")
+            forwards += tabulate.tabulate([r[0][0], r[0][1], ["", ""]+r[1], ["", ""]+r[2], ["", ""]+r[3], ["", ""]+r[4], ["", ""]+r[5]], headers = ["error", "sentence_number", "left_context", "location", "right_context"], tablefmt = "html")
         output_div.innerHTML = forwards
             
 
