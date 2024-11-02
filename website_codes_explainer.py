4dcc953e15fe2d6cff48c087396be5b82454ad93
sautedman <sautedman@protonmail.com>
2024-11-02 05:03:29 -0700

    more user friendly organization of the tables

--- website_codes_explainer.py
+++ website_codes_explainer.py
@@ -7,34 +7,32 @@ import pyscript
 import tabulate
 import grammar_codes
 
-codes_div = pyscript.document.querySelector("#codes")
-table = [["Code", "Technical Definition", "Informal Explanation", "Narrow/Broad Analysis"]]
+codes_narrow_broad_div = pyscript.document.querySelector("#codes_narrow_broad")
+codes_narrow_div = pyscript.document.querySelector("#codes_narrow")
+codes_broad_div = pyscript.document.querySelector("#codes_broad")
+table_narrow_broad = [["Code", "Technical Definition", "Informal Explanation"]]
+table_narrow = [["Code", "Technical Definition", "Informal Explanation"]]
+table_broad = [["Code", "Technical Definition", "Informal Explanation"]]
+
+def row_prep(header, explanation):
+    row =  [header]
+    if regex.search(r"\(.*\)", grammar_codes.abbreviations[x]):
+        row.append(regex.match(r"[^(]*", explanation)[0])
+        row.append(regex.search(r"\(.*\)", explanation)[0][1:-1])
+    elif not regex.search(r"\(.*\)", explanation):
+        row.append(explanation)
+        row.append("")
+    return row
 
 for x in sorted(grammar_codes.abbreviations):
-    row =  []
+    row = row_prep(x, grammar_codes.abbreviations[x])
     narrow_only = x not in grammar_codes.abbreviations_high
-    if narrow_only: row.append("Narrow Analyses Only")
-    if not narrow_only: row.append("Narrow and Broad Analyses")
-    if regex.search(r"\(.*\)", grammar_codes.abbreviations[x]):
-        row.append(regex.search(r"\(.*\)", grammar_codes.abbreviations[x])[0][1:-1])
-        row.append(regex.match(r"[^(]*", grammar_codes.abbreviations[x])[0])
-    elif not regex.search(r"\(.*\)", grammar_codes.abbreviations[x]):
-        row.append("")
-        row.append(grammar_codes.abbreviations[x])
-    row.append(x)
-    table.append(reversed(row))
+    if narrow_only: table_narrow.append(row)
+    if not narrow_only: table_narrow_broad.append(row)
 
 for x in sorted(grammar_codes.abbreviations_high):
-    row =  []
-    if x not in grammar_codes.abbreviations:
-        row.append("Broad Analyses Only")
-        if regex.search(r"\(.*\)", grammar_codes.abbreviations_high[x]):
-            row.append(regex.search(r"\(.*\)", grammar_codes.abbreviations_high[x])[0][1:-1])
-            row.append(regex.match(r"[^(]*", grammar_codes.abbreviations_high[x])[0])
-        elif not regex.search(r"\(.*\)", grammar_codes.abbreviations_high[x]):
-            row.append("")
-            row.append(grammar_codes.abbreviations_high[x])
-        row.append(x)
-        table.append(reversed(row))
+    if x not in grammar_codes.abbreviations: table_broad.append(row_prep(x, grammar_codes.abbreviations_high[x]))
 
-codes_div.innerHTML = tabulate.tabulate(table, tablefmt='html')
+codes_narrow_broad_div.innerHTML = tabulate.tabulate(table_narrow_broad, tablefmt='html')
+codes_narrow_div.innerHTML = tabulate.tabulate(table_narrow, tablefmt='html')
+codes_broad_div.innerHTML = tabulate.tabulate(table_broad, tablefmt='html')
