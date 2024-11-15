3d0a12a35e707d248f7e1d0ffa420764b8c4cde4
sautedman <sautedman@protonmail.com>
2024-11-15 10:30:46 -0800

    more intuitive complexity sort

--- main.py
+++ main.py
@@ -458,22 +458,8 @@ def parse_words_expanded(event):
     elif analysis_mode.value == "complexity":
         comp_counts = sc.alg_morph_counts(*sc.interface(pos_regex, *h["m_parse_lo"]))
         overall_score = sc.alg_morph_score_rate(*comp_counts)
-        itemized_scores = []
-        for x in comp_counts: itemized_scores.append(sc.alg_morph_score_rate(x))
-        s_score_pairs = sorted([x for x in zip(itemized_scores, h["original"])], key = lambda x: x[0])
-        sectioned = [["Overall Scores (Verb Category/Order/Features per Sentence): {0}/{1}/{2}".format(overall_score[0], overall_score[1], overall_score[2])]]
-        prev_vcat = []
-        prev_vord = []
-        for ssp in s_score_pairs:
-            new_vcat = ssp[0][0]
-            new_vord = ssp[0][1]
-            if new_vcat != prev_vcat or new_vord != prev_vord:
-                sectioned.append([">>Verb Category/Order Score: {0}/{1}".format(new_vcat, new_vord)])
-                sectioned.append(ssp[1])
-                prev_vcat = new_vcat
-                prev_vord = new_vord
-            else:
-                sectioned.append(ssp[1])
+        sectioned = [["Overall Score (Features per Sentence):",  str(overall_score[2])]]
+        for ssp in sorted([x for x in zip(comp_counts, h["original"])], key = lambda y: y[0][-1][0]): sectioned.append([ssp[1], ssp[0][-1][0])
         output_div.innerHTML = tabulate.tabulate(sectioned, tablefmt="html")
     elif analysis_mode.value == "glossary":
         pass
