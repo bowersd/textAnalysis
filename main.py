0ad9728f964524ad52f51724de03b1d54c7d4822
sautedman <sautedman@protonmail.com>
2024-11-15 09:11:42 -0800

    putting old complexity sort code under the new complexity sort

--- main.py
+++ main.py
@@ -460,7 +460,25 @@ def parse_words_expanded(event):
                 sectioned.append(ssp[1])
         output_div.innerHTML = tabulate.tabulate(sectioned, tablefmt="html")
     elif analysis_mode.value == "complexity":
-        pass
+        comp_counts = sc.alg_morph_counts(*sc.interface(pos_regex, *h["m_parse_lo"]))
+        overall_score = sc.alg_morph_score_rate(*comp_counts)
+        itemized_scores = []
+        for x in comp_counts: itemized_scores.append(sc.alg_morph_score_rate(x))
+        s_score_pairs = sorted([x for x in zip(itemized_scores, h["original"])], key = lambda x: x[0])
+        sectioned = [["Overall Scores (Verb Category/Order/Features per Sentence): {0}/{1}/{2}".format(overall_score[0], overall_score[1], overall_score[2])]]
+        prev_vcat = []
+        prev_vord = []
+        for ssp in s_score_pairs:
+            new_vcat = ssp[0][0]
+            new_vord = ssp[0][1]
+            if new_vcat != prev_vcat or new_vord != prev_vord:
+                sectioned.append([">>Verb Category/Order Score: {0}/{1}".format(new_vcat, new_vord)])
+                sectioned.append(ssp[1])
+                prev_vcat = new_vcat
+                prev_vord = new_vord
+            else:
+                sectioned.append(ssp[1])
+        output_div.innerHTML = tabulate.tabulate(sectioned, tablefmt="html")
     elif analysis_mode.value == "glossary":
         pass
     elif analysis_mode.value in ["triage", "reversed_triage"]:
