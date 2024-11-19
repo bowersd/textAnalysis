b7fc9e9e219532ef82cca28fe9ae16d1ca3987da
sautedman <sautedman@protonmail.com>
2024-11-18 22:09:44 -0800

    update time stamp, removed async declaration

--- main.py
+++ main.py
@@ -307,7 +307,7 @@ form_values = {
 #        "no_deletion_relaxed":{"order":"", "url":"https://raw.githubusercontent.com/bowersd/otw/releases/download/v.0.1.0-alpha/unsyncopated_analyzer_relaxed.hfstol",  "file":None}
 #        }
 
-async def cascade_customization(event):
+def cascade_customization(event):
     form_values["rhodes"]["order"] = pyscript.document.querySelector("#rhodes").value
     form_values["rhodes_relaxed"]["order"] = pyscript.document.querySelector("#rhodes_relaxed").value
     form_values["corbiere"]["order"] = pyscript.document.querySelector("#corbiere").value
