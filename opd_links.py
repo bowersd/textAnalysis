1fc743925096251beeb7ed8bec21a00e46e59e2f
bowersd <dustin.a.bowers@proton.me>
2025-08-09 07:37:57 -0700

    added a pprinted txt file for manual inspection of urls where the lexical information is possibly mis-typed

--- opd_links.py
+++ opd_links.py
@@ -2,28 +2,36 @@ import sys
 import regex
 
 def hyphens(string):
-    return "-".join(regex.split( "'| ",string)).lower()
+    return "-".join(regex.split( "'| ", regex.sub("(^')|('$)", "", string))).lower()
 
 def stitch(lemma, pos):
     if pos == "VTI" and lemma.endswith("oon"): return hyphens(lemma)+"-"+"vti2"
     elif pos == "VTI" and lemma.endswith("in"): return hyphens(lemma)+"-"+"vti3"
     elif pos == "VAI" and lemma.endswith("am"): return hyphens(lemma)+"-"+"vai2"
     elif pos == "VAIPL": return hyphens(lemma)+"-"+"vai"
+    elif pos == "VIIPL": return hyphens(lemma)+"-"+"vii"
     elif pos == "VAIO": return hyphens(lemma)+"-"+"vai-o"
     elif pos.startswith("ADV"): return hyphens(lemma)+"-"+pos[:3].lower()+"-"+pos[3:].lower()
     elif pos == "NUM": return hyphens(lemma)+"-"+"adv"+"-"+"num"
     elif pos.startswith("PC"): return hyphens(lemma)+"-"+pos[:2].lower()+"-"+pos[2:].lower()
-    elif pos.startswith("PRON"): return hyphens(lemma)+"-"+pos[:5].lower()+"-"+pos[5:].lower()
-    elif pos.startswith("Name"): return hyphens(lemma)+"-"+pos[:5].lower()+"-"+pos[5:].lower()
+    elif pos.startswith("PRON"): return hyphens(lemma)+"-"+pos[:4].lower()+"-"+pos[4:].lower()
+    elif pos.startswith("Name"): return hyphens(lemma)+"-"+pos[:4].lower()+"-"+pos[4:].lower()
     else: return hyphens(lemma)+"-"+pos.lower()
 
 if __name__ == "__main__":
-    with open("opd_manual_links.csv", 'w') as f_out:
-        for f in sys.argv[1:]:
-            with open(f) as df:
-                for line in df:
-                    x = line.strip().split(',')
-                    y = "https://ojibwe.lib.umn.edu/main-entry/"+stitch(x[0], x[2]) 
-                    if y != x[-1]: 
-                        print(y)
-                        print(x[-1], x[0], x[2])
+    with open("opd_url_typo_check.txt", 'w') as ff_out:
+        with open("opd_manual_links.csv", 'w') as f_out:
+            for f in sys.argv[1:]:
+                with open(f) as df:
+                    for line in df:
+                        x = line.strip().split(',')
+                        stchd = stitch(x[0], x[2])
+                        y = "https://ojibwe.lib.umn.edu/main-entry/"+stchd
+                        if y != x[-1]: f_out.write(",".join([x[0], x[2], x[-1], '\n']))
+                        pre = len("https://ojibwe.lib.umn.edu/main-entry/")
+                        if stchd != x[-1][pre:pre+len(stchd)]:
+                            ff_out.write(stchd+'\n')
+                            ff_out.write(x[-1][pre:pre+len(stchd)]+'\n')
+                            ff_out.write(x[-1]+'\n')
+                            ff_out.write('\n')
+                            
