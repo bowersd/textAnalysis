5635c3e6cbeded3b8cbca46c1059f040c6844268
bowersd <sautedman@protonmail.com>
2025-08-12 08:28:52 -0700

    fixing inverted arguments

--- main.py
+++ main.py
@@ -487,14 +487,14 @@ def parse_words_expanded(event):
         lem_links = []
         for i in range(len(local)):
             if model_credit[sep_punct(line, True).split()[i]] == "./morphophonology_analyze_border_lakes.hfstol": 
-                lem = extract_lemma(ciw_pos_regex, local[i])
-                pos = extract_pos(ciw_pos_regex, local[i])
+                lem = extract_lemma(local[i], ciw_pos_regex)
+                pos = extract_pos(local[i], ciw_pos_regex)
                 print(local[i], lem, pos)
                 lemms.append(lem)
                 if (lem, pos) in opd_manual_links: lem_links.append(opd.wrap_opd_url(opd_manual_links[(lem, pos)], lem)) 
                 else: lem_links.append(opd.wrap_opd_url(opd.mk_opd_url(lem, pos), lem)) 
             else: 
-                lem = extract_lemma(pos_regex, local[i])
+                lem = extract_lemma(local[i], pos_regex)
                 lemms.append(lem)
                 lem_links.append(wrap_nod_entry_url(lem, **iddict))
         h["lemmata"].append(lemms) #converted to links in interlinearize, kept plain in freq count ... should always have links available, methinks
