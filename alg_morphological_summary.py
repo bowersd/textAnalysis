c205740779d765bc792580ff7c0fe8a5ebc7e1ab
bowersd <sautedman@protonmail.com>
2025-08-22 06:56:19 -0700

    changed Obv retrieval behavior so that obviatives will not be summarized as plurals, also caught a bug that would have made S information show up in O

--- alg_morphological_summary.py
+++ alg_morphological_summary.py
@@ -58,8 +58,8 @@ def interpret_ciw(analysis_in, postags):
         #elif x == "Incl": pass #this is person information for pronouns, which we do not have a good place for at the moment
         elif x.endswith("Poss") and x[0] in ["1", "2", "3"]: 
             summary["S"]["Pers"] = x[0]
-            if x[1:3] == "Pl": summary["S"]["Num"] = "Pl" #sg is not retained, also obv/prox is not retained when preceded by Pl
-            elif x[1:6] == "SgObv": summary["S"]["Num"] = "Obv" #sg is not retained, also obv/prox is not retained when preceded by Pl
+            if x[1:6] == "PlObv": summary["S"]["Num"] = "Obv" #sg/prox is not retained. this specific combination is targeted to retrieve Obv because that will come first in lexicographic sort of ambiguous analyses, and will be selected by disambiguation
+            elif x[1:3] == "Pl": summary["S"]["Num"] = "Pl" 
         elif x == "ExclPoss": 
             summary["S"]["Pers"] = "1"
             summary["S"]["Num"] = "Pl"
@@ -68,12 +68,12 @@ def interpret_ciw(analysis_in, postags):
             summary["S"]["Num"] = "1Pl"
         elif x.endswith("Head"): #Participle information
             summary["Pcp"]["Pers"] = x[0]
-            if x[1:3] == "Pl": summary["Pcp"]["Num"] = "Pl"
-            elif x[1:6] == "SgObv": summary["Pcp"]["Num"] = "Obv" #sg is not retained, also obv/prox is not retained when preceded by Pl
+            if x[1:6] == "PlObv": summary["Pcp"]["Num"] = "Obv"#sg/prox is not retained. this specific combination is targeted to retrieve Obv because that will come first in lexicographic sort of ambiguous analyses, and will be selected by disambiguation
+            elif x[1:3] == "Pl": summary["Pcp"]["Num"] = "Pl" 
         elif x.endswith("Obj") and x[0] in ["0", "1", "2", "3", "X"]:
             summary["O"]["Pers"] = x[0]
-            if x[1:3] == "Pl": summary["O"]["Num"] = "Pl" #sg is not retained, also obv/prox is not retained when preceded by Pl
-            elif x[1:6] == "SgObv": summary["O"]["Num"] = "Obv" #sg is not retained, also obv/prox is not retained when preceded by Pl
+            if x[1:6] == "PlObv": summary["O"]["Num"] = "Obv" #sg/prox is not retained. this specific combination is targeted to retrieve Obv because that will come first in lexicographic sort of ambiguous analyses, and will be selected by disambiguation
+            elif x[1:3] == "Pl": summary["O"]["Num"] = "Pl" 
         elif x == "ExclObj": 
             summary["O"]["Pers"] = "1"
             summary["O"]["Num"] = "Pl"
@@ -81,9 +81,9 @@ def interpret_ciw(analysis_in, postags):
             summary["O"]["Pers"] = "2"
             summary["O"]["Num"] = "1Pl"
         elif x.endswith("Subj") and x[0] in ["0", "1", "2", "3", "X"]:
-            summary["O"]["Pers"] = x[0]
-            if x[1:3] == "Pl": summary["O"]["Num"] = "Pl" #sg is not retained, also obv/prox is not retained when preceded by Pl
-            elif x[1:6] == "SgObv": summary["O"]["Num"] = "Obv" #sg is not retained, also obv/prox is not retained when preceded by Pl
+            summary["S"]["Pers"] = x[0]
+            if x[1:6] == "PlObv": summary["S"]["Num"] = "Obv" #sg/prox is not retained. this specific combination is targeted to retrieve Obv because that will come first in lexicographic sort of ambiguous analyses, and will be selected by disambiguation
+            elif x[1:3] == "Pl": summary["S"]["Num"] = "Pl" 
         elif x == "ExclSubj": 
             summary["S"]["Pers"] = "1"
             summary["S"]["Num"] = "Pl"
