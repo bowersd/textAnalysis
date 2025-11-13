b20ce4fa807d9053a3dec40acaacd8e86b9423ad
bowersd <sautedman@protonmail.com>
2025-11-13 11:49:19 -0800

    commented on an inelegance in the linking of lemmata to dictionary urls

--- main.py
+++ main.py
@@ -711,7 +711,7 @@ def parse_words_expanded(event):
         #freqs_out = tabulate.tabulate(header + nu_cnts, tablefmt='html')
         #output_div.innerHTML = freqs_out
         freqd = frequency_count(h)
-        output_div.innerHTML = frequency_format(freqd[0], freqd[1]) 
+        output_div.innerHTML = frequency_format(freqd[0], freqd[1]) #can't just apply to frequency_count() value, bc f_c() returns [{lemmas:{toks:cnt}}, lemmas:links}]. {lemmas:links} is formed during the iteration through the sentences. To make it cleanly (further upstream), you need (analyzer, lemma, pos) to make {lemmas:links}
     elif analysis_mode.value == "verb_sort":
         comp_counts = sc.alg_morph_counts(*sc.interface(pos_regex, *h["m_parse_lo"]))
         c_order = ["VTA", "VAIO", "VTI", "VAI", "VII", "(No verbs found)"] #need to specify order in order to sort by count of verb in the relevant category
