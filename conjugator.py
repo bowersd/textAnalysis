ad49a7f7e87c600987b660e4dea78cc64a34eeef
bowersd <dustin.a.bowers@proton.me>
2025-12-18 07:16:25 -0800

    navigating funny corners of phi-features (VAI 3rd persons including 3+2Pl in independent prt dub)

--- conjugator.py
+++ conjugator.py
@@ -3,6 +3,13 @@ import regex
 def lemma_insert(lemma, tag_skeleton):
     return regex.sub(r'<>', lemma, tag_skeleton)
 
+def recreate_number_tags(person, number, prefix):
+    if person == "1" and number == "Pl": return "1Pl"
+    elif person == "2" and number == "Pl": return "2Pl"
+    elif person == "2" and number == "1Pl": return "1Pl"
+    elif person == "3" and number == "Pl" and prefix: return "2Pl"
+    elif person == "3" and number == "Pl" and not prefix: return "3Pl"
+
 def tag_assemble(**broad_analysis):
     algonquianized = {"person_prefix": [],
                       "preverbs": [], 
@@ -19,6 +26,7 @@ def tag_assemble(**broad_analysis):
                       "pejorative": [],
                       "contemptive": [],
                      }
+    algonquanized["POS"] = broad_analysis["Head"]
     algonquanized["order"] = broad_analysis["Order"]
     algonquanized["negation"] = broad_analysis["Neg"]
     algonquanized["mode"] = broad_analysis["Mode"]
@@ -26,6 +34,8 @@ def tag_assemble(**broad_analysis):
         pass #special behavior for imperatives and conjuncts
     elif broad_analysis["S"]["Pers"] != '0': #inanimate subjects (VTA, VII) are special
         inversion = False
+        if algonquianized["POS"].startswith("VAI") and broad_analysis["S"]["Pers"] == "3": #including VAIOs
+            pass #need to make suffixes, also 3+2Pl if independent pret dub
         algonquianized["person_prefix"] = broad_analysis["S"]["Pers"]
         algonquianized["prefix_number"] = broad_analysis["S"]["Num"] #standard outputs from interpret() are just Pl instead of 3Pl, need to restore full tag
         hierarchy = {"1":2, "2":1, "3":3, "0": 4, "":5} #VAIOs?
