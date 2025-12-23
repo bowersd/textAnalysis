05160c14752fedc054aad3f13268a55a511fb5cb
bowersd <dustin.a.bowers@proton.me>
2025-12-23 10:35:04 -0800

    integrated vta cnj

--- conjugator.py
+++ conjugator.py
@@ -151,6 +151,7 @@ def vta_adjustments(**broad_analysis)
     check_for_person_ties(**broad_analysis)
     if broad_analysis["order"] == "Cnj":
         h["theme_sign"] = vta_cnj_theme_update(**broad_analysis)
+        h["prefix_number"] = vta_cnj_theme_continuation(h["theme_sign"], **broad_analysis) #independent prefix_number is in an analogous spot to the cnj argument elaborations
     if broad_analysis["order"] == "Imp":
         pass
     else:
