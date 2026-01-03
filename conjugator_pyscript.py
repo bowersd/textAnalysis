fb59e9d9f2518d790401086e6dd0b06e368ea479
bowersd <dustin.a.bowers@proton.me>
2026-01-03 07:24:40 -0800

    script for tying conjugator functions to html ux

---
+++ conjugator_pyscript.py
@@ -0,0 +1,22 @@
+import micropip
+await micropip.install(
+    'https://files.pythonhosted.org/packages/e0/02/c10a69ff21d6679a6b6e28c42cd265bec2cdd9be3dcbbee830a10fa4b0e5/pyhfst-1.3.0-py2.py3-none-any.whl'
+)
+import pyscript
+import asyncio
+import conjugator
+
+#catch parameters from ux
+
+form_values = {"lemma":"",
+               "S":{"Pers":"", "Num":""}, 
+               "O":{"Pers":"", "Num":""}, 
+               "Head":"", 
+               "Order":"", 
+               "Neg":"", 
+               "Mode":[], 
+               "Periph":"", 
+               "ConDim":"", 
+               "PosTheme":"", 
+               "Pejorative":"" } 
+
