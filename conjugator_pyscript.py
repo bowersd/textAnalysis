ef2d29ef0367b100cf815b4bbc58fd75d3adbad8
bowersd <dustin.a.bowers@proton.me>
2026-01-03 13:16:21 -0800

    fleshing out the interface

--- conjugator_pyscript.py
+++ conjugator_pyscript.py
@@ -24,5 +24,20 @@ def inflect_word(event):
     form_values["Lemma"] = pyscript.document.querySelector("#lemma").value
     form_values["Head"] = pyscript.document.querySelector("#POS").value
     form_values["Order"] = pyscript.document.querySelector("#Order").value
+    prt = pyscript.document.querySelector("#Mode:Prt")
+    dub = pyscript.document.querySelector("#Mode:Dub")
+    neg = pyscript.document.querySelector("#Neg")
     sub = pyscript.document.querySelector("#S")
     obj = pyscript.document.querySelector("#O")
+    if form_values["Head"].startswith("N"): 
+        sub = pyscript.document.querySelector("#Possessor")
+        per = pyscript.document.querySelector("#Periph")
+        form_values["ConDim"] = pyscript.document.querySelector("#ConDim")
+        form_values["PosTheme"] = pyscript.document.querySelector("#PosThm")
+        form_values["Pejorative"] = pyscript.document.querySelector("#Pej")
+    if sub: 
+        form_values["S"]["Pers"] = sub[0]
+        form_values["S"]["Num"] = sub[1:]
+    if obj: 
+        form_values["O"]["Pers"] = sub[0]
+        form_values["O"]["Num"] = sub[1:]
