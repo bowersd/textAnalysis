5a348e0ea7d811daa0b8890e619597431d3ab07a
bowersd <dustin.a.bowers@proton.me>
2026-01-22 06:48:46 -0800

    preparing to build a prefix trie

---
+++ trie.py
@@ -0,0 +1,17 @@
+
+def rules(*lexicon):
+    r = []
+    total = 0
+    for w in lexicon:
+        state = 0
+        for c in w:
+            prev = state
+            nxt = total+1
+            r.append([state, (c, nxt), 1])
+            total = nxt
+            state = nxt
+        r.append([state, (,), 1])
+    return r
+
+if __name__ == "__main__":
+    for r in rules("cat", "call", "dog"): print(r)
