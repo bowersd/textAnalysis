053a34a424d60dbe4824f1244ebad46e0bf784b5
bowersd <dustin.a.bowers@proton.me>
2026-02-06 22:28:14 -0800

    removing debugging print statements

--- main.py
+++ main.py
@@ -548,20 +548,10 @@ def frequency_format(lemmata_data):
 def take_windows(sentence_data, size, *addresses):
     windows = []
     for a in addresses:
-        print(a)
         w = {}
         left_edge = 0
         if a[1]-size > 0: left_edge = a[1]-size
-        print("###sentence data")
-        for sd in sentence_data: 
-            print("###data field")
-            print(sd)
-            print([sentence_data[sd]])
-            print("###sentence data -> line")
-            print([sentence_data[sd][a[0]]])
-            print("###sentence data -> line, word")
-            print([sentence_data[sd][a[0]][left_edge:a[1]+size+1]])
-            w[sd] = [sentence_data[sd][a[0]][left_edge:a[1]+size+1]]
+        for sd in sentence_data: w[sd] = [sentence_data[sd][a[0]][left_edge:a[1]+size+1]]
         windows.append(w)
     return windows
 
