077ba12ee05f555dafee5f029a7cd11f30e12f4e
bowersd <dustin.a.bowers@proton.me>
2026-03-06 14:02:29 -0800

    getting closer...

--- main.py
+++ main.py
@@ -655,7 +655,7 @@ def verb_collation_format(lemmata_data):
         if lemmata_data[lem]["pos"] in verbcats:
             for t in lemmata_data[lem]["tokens"]:
                 print("row",(t, lemmata_data[lem]["tokens"][t]["m_parse_hi"], lemmata_data[lem]["tokens"][t]["exe"]))
-                verbdict[lemmata_data[lem]["pos"]].append((t, lemmata_data[lem][t]["tokens"]["m_parse_hi"], lemmata_data[lem]["tokens"][t]["exe"])) 
+                verbdict[lemmata_data[lem]["pos"]].append((t, lemmata_data[lem]["tokens"][t]["m_parse_hi"], lemmata_data[lem]["tokens"][t]["exe"])) 
     for c in verbcats:
         preamble = "<p>Found these verbs of category {0}:</p>\n".format(c)
         header = "<table>\n<tbody>\n<tr>\n<td>"+"</td>\n<td>".join(["Word", "Broad Analysis", "Show/Hide Examples"])+"</td>\n</tr>\n"
